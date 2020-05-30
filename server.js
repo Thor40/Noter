3be4e57bda3ad1d0544204e46c986594946957cd
Thor40 <nicholas.kosik@gmail.com>
2020-05-29 19:13:16 -0500

    findId, Port, get, Heroku

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const app = express();
 const { notes } = require('./Develop/db/db')
 const PORT = process.env.PORT || 3001;
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+};
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
+});
+
+app.get('/api/notes/:id', (req,res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
 });
 
 app.listen(PORT, () => {
