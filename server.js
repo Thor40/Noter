34bbeff7e2d45bf44321dc28b2bfe2cb5cb3ef45
Thor40 <nicholas.kosik@gmail.com>
2020-05-29 19:44:37 -0500

    POST

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const { notes } = require('./Develop/db/db')
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
 };
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
@@ -21,6 +34,19 @@ app.get('/api/notes/:id', (req,res) => {
       } else {
         res.send(404);
       }
+});
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of array
+    req.body.id = notes.length.toString();
+    // add note to json file and note array
+    const note = createNewNote(req.body, notes);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
